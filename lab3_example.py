3535dc726463ff4e8f8225e2d5c035c7a673c6ac
zuzuz <duda.zuzanna7@gmail.com>
2025-01-17 13:30:55 +0100

    lab 3 initial commit with example code

---
+++ lab3_example.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+import sys
+
+from glfw.GLFW import *
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+
+def startup():
+    update_viewport(None, 1000, 1000)
+    glClearColor(0.34, 0.0, 0.0, 1.0)
+    # mechanizm bufora głębi
+    glEnable(GL_DEPTH_TEST)
+
+
+def shutdown():
+    pass
+
+# rysujemy układ współrzędnych
+def axes():
+    glBegin(GL_LINES)
+
+    # oś x
+    glColor3f(1.0, 0.0, 0.0)
+    glVertex3f(-5.0, 0.0, 0.0)
+    glVertex3f(5.0, 0.0, 0.0)
+
+    # oś y
+    glColor3f(0.0, 1.0, 0.0)
+    glVertex3f(0.0, -5.0, 0.0)
+    glVertex3f(0.0, 5.0, 0.0)
+
+    # oś z
+    glColor3f(0.0, 0.0, 1.0)
+    glVertex3f(0.0, 0.0, -5.0)
+    glVertex3f(0.0, 0.0, 5.0)
+
+    glEnd()
+
+# zanimowanie obiektu i obrót
+def spin(angle):
+    glRotatef(angle, 1.0, 0.0, 0.0)
+    glRotatef(angle, 0.0, 1.0, 0.0)
+    glRotatef(angle, 0.0, 0.0, 1.0)
+
+
+def render(time):
+
+    # mechanizm bufora głębi
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+    glLoadIdentity()
+
+    spin(time * 180/3.1415)
+    axes()
+
+    glFlush()
+
+
+def update_viewport(window, width, height):
+    if width == 0:
+        width = 1
+    if height == 0:
+        height = 1
+    aspect_ratio = width / height
+
+    glMatrixMode(GL_PROJECTION)
+    glViewport(0, 0, width, height)
+    glLoadIdentity()
+
+    # zmieniony zakres rysowania
+    if width <= height:
+        glOrtho(-7.5, 7.5, -7.5 / aspect_ratio, 7.5 / aspect_ratio, 7.5, -7.5)
+    else:
+        glOrtho(-7.5 * aspect_ratio, 7.5 * aspect_ratio, -7.5, 7.5, 7.5, -7.5)
+
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+
+
+def main():
+    if not glfwInit():
+        sys.exit(-1)
+
+    window = glfwCreateWindow(1000, 1000, __file__, None, None)
+    if not window:
+        glfwTerminate()
+        sys.exit(-1)
+
+    glfwMakeContextCurrent(window)
+    glfwSetFramebufferSizeCallback(window, update_viewport)
+    glfwSwapInterval(1)
+
+    startup()
+    while not glfwWindowShouldClose(window):
+        render(glfwGetTime())
+        glfwSwapBuffers(window)
+        glfwPollEvents()
+    shutdown()
+
+    glfwTerminate()
+
+
+if __name__ == '__main__':
+    main()
